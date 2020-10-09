60554dea6e5f3995df42072bb3b5631eb94115ac
Alxts <alexisgonem@gmail.com>
2020-10-09 16:13:16 -0600

    lectura de datos desde python

---
+++ lecturadedatos.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+nombre = raw_input("Bienvenido, ingrese sus credenciales: ") # lectura de variables alfanumericas
+
+num1 = input("ingrese un numero: ")# input es una funcion, solo para variables numericas
+
+num2 = input("ingrese un segundo numero: ")
+
+suma = num1 + num2
+
+print "que pex ",nombre
+print "la suma es: ",suma
